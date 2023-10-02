3992c8f588d2c8d14862f09398a8e6d5bc17ef20
CorySonderman <sonderman2000@yahoo.com>
2023-10-02 11:41:07 -0500

    Final draft of exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -6,24 +6,29 @@ namespace SelectionStatementsExercise2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What\'s your favorite school subject?");
+            Console.WriteLine("What\'s your favorite subject in?");
             var subject  = (Console.ReadLine());
             Console.WriteLine();
             switch (subject)
             {
-                case  ("math"):
+                case "math":
+                case "Math":
                     Console.WriteLine("I like Math too!");
                     break;
-                case ("science"):
+                case "science":
+                case "Science":
                     Console.WriteLine("I like Science too!");
                     break;
-                case ("biology"):
+                case "biology":
+                case "Biology":
                     Console.WriteLine("I like biology too!");
                     break;
-                case ("gym"):
+                case "gym":
+                case "Gym":
                     Console.WriteLine("I like Gym too!");
                    break;
-                case ("art"):
+                case "art":
+                case "Art":
                     Console.WriteLine("I like Art too!");
                     break;
                 default:
