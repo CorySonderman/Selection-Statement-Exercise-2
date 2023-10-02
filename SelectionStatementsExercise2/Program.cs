75c6958e7bb15f178e00bf2a2b497f75d4dae185
CorySonderman <sonderman2000@yahoo.com>
2023-10-02 08:22:57 -0500

    Completed Selection Statements exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Linq.Expressions;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What\'s your favorite school subject?");
+            var subject  = (Console.ReadLine());
+            Console.WriteLine();
+            switch (subject)
+            {
+                case  ("math"):
+                    Console.WriteLine("I like Math too!");
+                    break;
+                case ("science"):
+                    Console.WriteLine("I like Science too!");
+                    break;
+                case ("biology"):
+                    Console.WriteLine("I like biology too!");
+                    break;
+                case ("gym"):
+                    Console.WriteLine("I like Gym too!");
+                   break;
+                case ("art"):
+                    Console.WriteLine("I like Art too!");
+                    break;
+                default:
+                    Console.WriteLine("I like most subjects in school because I " +
+                        "like to learn.");
+                    break;
+            }
         }
+
     }
 }
